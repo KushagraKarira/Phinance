329dd0447b02ce222d3d9188d1c9609d0b2ea392
Kushagra Karira <kushagrakarira@gmail.com>
2023-03-15 00:38:08 +0530

    US only

--- balance-sheet.js
+++ balance-sheet.js
@@ -1,11 +1,13 @@
 function getBalanceSheet() {
   const companySymbol = document.getElementById("companySymbol").value;
-  const url = `https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/${companySymbol}?apikey=5b38085c4451c128b9b0919a0dffab8f`;
+  const apiKey = "5b38085c4451c128b9b0919a0dffab8f";
+  const url = `https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/${companySymbol}?apikey=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const balanceSheet = data.financials[0];
+      // Extract the latest balance sheet data
+      const balanceSheet = data.financials[data.financials.length - 1];
 
       // Create a new table element
       const table = document.createElement("table");
@@ -13,48 +15,22 @@ function getBalanceSheet() {
       // Create the header row
       const headerRow = document.createElement("tr");
       headerRow.innerHTML = `
-        <th>Date</th>
-        <th>Cash and Equivalents</th>
-        <th>Short-term Investments</th>
-        <th>Accounts Receivable</th>
-        <th>Total Current Assets</th>
-        <th>Property, Plant and Equipment Net</th>
-        <th>Long-term Investments</th>
-        <th>Goodwill and Intangible Assets</th>
-        <th>Total Assets</th>
-        <th>Current Liabilities</th>
-        <th>Long-term Debt</th>
-        <th>Total Liabilities</th>
-        <th>Common Stock</th>
-        <th>Retained Earnings</th>
-        <th>Treasury Stock</th>
-        <th>Shareholders Equity</th>
-        <th>Net Tangible Assets</th>
+        <th>Item</th>
+        <th>Value</th>
       `;
       table.appendChild(headerRow);
 
-      // Create a row for the balance sheet data
-      const balanceSheetRow = document.createElement("tr");
-      balanceSheetRow.innerHTML = `
-        <td>${balanceSheet.date}</td>
-        <td>${balanceSheet.cashAndShortTermInvestments}</td>
-        <td>${balanceSheet.shortTermInvestments}</td>
-        <td>${balanceSheet.accountsReceivable}</td>
-        <td>${balanceSheet.totalCurrentAssets}</td>
-        <td>${balanceSheet.propertyPlantEquipmentNet}</td>
-        <td>${balanceSheet.longTermInvestments}</td>
-        <td>${balanceSheet.goodwillAndIntangibleAssets}</td>
-        <td>${balanceSheet.totalAssets}</td>
-        <td>${balanceSheet.currentLiabilities}</td>
-        <td>${balanceSheet.longTermDebt}</td>
-        <td>${balanceSheet.totalLiabilities}</td>
-        <td>${balanceSheet.commonStock}</td>
-        <td>${balanceSheet.retainedEarnings}</td>
-        <td>${balanceSheet.treasuryStock}</td>
-        <td>${balanceSheet.shareholderEquity}</td>
-        <td>${balanceSheet.netTangibleAssets}</td>
-      `;
-      table.appendChild(balanceSheetRow);
+      // Create rows for each balance sheet item
+      for (const item in balanceSheet) {
+        if (item !== "date") {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+            <td>${item}</td>
+            <td>${balanceSheet[item]}</td>
+          `;
+          table.appendChild(row);
+        }
+      }
 
       // Replace any existing table with the new one
       const existingTable = document.getElementById("balanceSheet");
