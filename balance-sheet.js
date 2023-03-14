b8c9bfcb4816fa4cae0d6198b84485478db17109
Kushagra Karira <kushagrakarira@gmail.com>
2023-03-14 22:16:38 +0530

    show error

--- balance-sheet.js
+++ balance-sheet.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const tbody = document.querySelector('tbody');
+const errorContainer = document.querySelector('.error-container');
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -7,27 +8,38 @@ form.addEventListener('submit', async (event) => {
   const ticker = event.target.elements.ticker.value.toUpperCase();
 
   try {
-    const response = await fetch(`https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${ticker}&apikey=DLQSQO7DUNU4FN2E`);
-    const data = await response.json();
+    const response = await fetch(`https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${ticker}&apikey=YOUR_API_KEY`);
 
-    // Clear existing rows from table
-    tbody.innerHTML = '';
+    if (response.status === 200) {
+      const data = await response.json();
 
-    // Loop through balance sheet items and add rows to table
-    for (const [item, value] of Object.entries(data.annualReports[0])) {
-      if (item !== 'fiscalDateEnding') {
-        const row = document.createElement('tr');
-        const itemCell = document.createElement('td');
-        itemCell.textContent = item;
-        const valueCell = document.createElement('td');
-        valueCell.textContent = value;
-        row.appendChild(itemCell);
-        row.appendChild(valueCell);
-        tbody.appendChild(row);
+      // Clear existing rows from table
+      tbody.innerHTML = '';
+
+      // Loop through balance sheet items and add rows to table
+      for (const [item, value] of Object.entries(data.annualReports[0])) {
+        if (item !== 'fiscalDateEnding') {
+          const row = document.createElement('tr');
+          const itemCell = document.createElement('td');
+          itemCell.textContent = item;
+          const valueCell = document.createElement('td');
+          valueCell.textContent = value;
+          row.appendChild(itemCell);
+          row.appendChild(valueCell);
+          tbody.appendChild(row);
+        }
       }
+
+      // Hide error message if previously displayed
+      errorContainer.style.display = 'none';
+    } else {
+      throw new Error('API request failed');
     }
   } catch (error) {
     console.error(error);
+
+    // Display error message
+    errorContainer.textContent = `Error: ${error.message}`;
+    errorContainer.style.display = 'block';
   }
 });
-
