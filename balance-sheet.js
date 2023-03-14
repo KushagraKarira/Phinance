c116c7191be8c741afd162960e607290f82972d3
Kushagra Karira <kushagrakarira@gmail.com>
2023-03-15 00:14:32 +0530

    fuck

--- balance-sheet.js
+++ balance-sheet.js
@@ -1,46 +1,68 @@
-const form = document.querySelector('form');
-const tbody = document.querySelector('tbody');
-const errorContainer = document.querySelector('.error-container');
+function getBalanceSheet() {
+  const companySymbol = document.getElementById("companySymbol").value;
+  const url = `https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/${companySymbol}?apikey=5b38085c4451c128b9b0919a0dffab8f`;
 
-const apiKey = "DLQSQO7DUNU4FN2E"; // replace with your Alpha Vantage API key
-const apiUrl = "https://www.alphavantage.co/query";
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const balanceSheet = data.financials[0];
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
+      // Create a new table element
+      const table = document.createElement("table");
 
-  const ticker = event.target.elements.ticker.value.toUpperCase();
+      // Create the header row
+      const headerRow = document.createElement("tr");
+      headerRow.innerHTML = `
+        <th>Date</th>
+        <th>Cash and Equivalents</th>
+        <th>Short-term Investments</th>
+        <th>Accounts Receivable</th>
+        <th>Total Current Assets</th>
+        <th>Property, Plant and Equipment Net</th>
+        <th>Long-term Investments</th>
+        <th>Goodwill and Intangible Assets</th>
+        <th>Total Assets</th>
+        <th>Current Liabilities</th>
+        <th>Long-term Debt</th>
+        <th>Total Liabilities</th>
+        <th>Common Stock</th>
+        <th>Retained Earnings</th>
+        <th>Treasury Stock</th>
+        <th>Shareholders Equity</th>
+        <th>Net Tangible Assets</th>
+      `;
+      table.appendChild(headerRow);
 
-  try {
-    const response = await fetch(`${apiUrl}?function=BALANCE_SHEET&symbol=${ticker}.NS&apikey=${apiKey}`);
-    if (response.status === 200) {
-      const data = await response.json();
+      // Create a row for the balance sheet data
+      const balanceSheetRow = document.createElement("tr");
+      balanceSheetRow.innerHTML = `
+        <td>${balanceSheet.date}</td>
+        <td>${balanceSheet.cashAndShortTermInvestments}</td>
+        <td>${balanceSheet.shortTermInvestments}</td>
+        <td>${balanceSheet.accountsReceivable}</td>
+        <td>${balanceSheet.totalCurrentAssets}</td>
+        <td>${balanceSheet.propertyPlantEquipmentNet}</td>
+        <td>${balanceSheet.longTermInvestments}</td>
+        <td>${balanceSheet.goodwillAndIntangibleAssets}</td>
+        <td>${balanceSheet.totalAssets}</td>
+        <td>${balanceSheet.currentLiabilities}</td>
+        <td>${balanceSheet.longTermDebt}</td>
+        <td>${balanceSheet.totalLiabilities}</td>
+        <td>${balanceSheet.commonStock}</td>
+        <td>${balanceSheet.retainedEarnings}</td>
+        <td>${balanceSheet.treasuryStock}</td>
+        <td>${balanceSheet.shareholderEquity}</td>
+        <td>${balanceSheet.netTangibleAssets}</td>
+      `;
+      table.appendChild(balanceSheetRow);
 
-      // Clear existing rows from table
-      tbody.innerHTML = '';
+      // Replace any existing table with the new one
+      const existingTable = document.getElementById("balanceSheet");
+      existingTable.replaceWith(table);
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Error getting balance sheet data. Please check the company symbol and API key.");
+    });
+}
 
-      // Loop through balance sheet items and add rows to table
-      for (const [item, value] of Object.entries(data.annualReports[0])) {
-        if (item !== 'fiscalDateEnding') {
-          const row = document.createElement('tr');
-          const itemCell = document.createElement('td');
-          itemCell.textContent = item;
-          const valueCell = document.createElement('td');
-          valueCell.textContent = value;
-          row.appendChild(itemCell);
-          row.appendChild(valueCell);
-          tbody.appendChild(row);
-        }
-      }
-
-      // Hide error message if previously displayed
-      errorContainer.style.display = 'none';
-    } else {
-      throw new Error('API Request failed');
-    }
-  } catch (error) {
-    console.error(error);
-    // Display error message
-    errorContainer.textContent = `Error: ${error.message}`;
-    errorContainer.style.display = 'block';
-  }
-});
